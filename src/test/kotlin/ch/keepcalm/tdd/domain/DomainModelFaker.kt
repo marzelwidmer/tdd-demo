8975e534ad107733715122219fba80e86197410c
marzelwidmer <marzelwidmer@gmail.com>
2021-05-23 22:54:01 +0200

    Add javafaker library

--- src/test/kotlin/ch/keepcalm/tdd/domain/DomainModelFaker.kt
+++ src/test/kotlin/ch/keepcalm/tdd/domain/DomainModelFaker.kt
@@ -2,34 +2,26 @@ package ch.keepcalm.tdd.domain
 
 import ch.keepcalm.tdd.domain.address.model.*
 import ch.keepcalm.tdd.domain.person.model.Person
+import com.github.javafaker.Faker
+
+
+
 
 class DomainModelFaker {
-
     companion object {
-        //        fun person(name: String = Faker().name.firstName(), email: String = Faker().internet.safeEmail()): Person = Person(
-//            name = name,
-//            email = email
-//        )
-//        fun address(): Address = Address(
-//            street = Street(Faker().address.streetName()),
-//            streetNr = StreetNr(Faker().address.buildingNumber()),
-//            zip = Zip(Faker().address.postcode()),
-//            city = City(Faker().address.city()),
-//            province = Province(Faker().address.cityWithState()),
-//            provinceCode = ProvinceCode(Faker().address.postcode())
-//        )
-        fun person(name: String = "John", email: String = "john@doe.com"): Person = Person(
+        private val  faker = Faker()
+        fun person(name: String = faker.name().firstName(), email: String = faker.internet().emailAddress()): Person = Person(
             name = name,
             email = email
         )
 
         fun address(): Address = Address(
-            street = Street("street"),
-            streetNr = StreetNr("strNr123"),
-            zip = Zip("8009"),
-            city = City("Zuerich"),
-            province = Province("Zuerich"),
-            provinceCode = ProvinceCode("ZH")
+            street = Street(faker.address().streetAddress()),
+            streetNr = StreetNr(faker.address().streetAddressNumber()),
+            zip = Zip(faker.address().zipCode()),
+            city = City(faker.address().city()),
+            province = Province(faker.address().state()),
+            provinceCode = ProvinceCode(faker.address().cityPrefix())
         )
     }
 }
