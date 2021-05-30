09fd37b76cd6e142e9c703fc96cec097705a21f5
marzelwidmer <marzelwidmer@gmail.com>
2021-05-30 17:08:40 +0200

    Remove Person API from IndexResource

--- src/main/kotlin/ch/keepcalm/tdd/domain/api/IndexResource.kt
+++ src/main/kotlin/ch/keepcalm/tdd/domain/api/IndexResource.kt
@@ -1,6 +1,5 @@
 package ch.keepcalm.tdd.domain.api
 
-import ch.keepcalm.tdd.domain.person.model.Person
 import kotlinx.coroutines.reactive.awaitSingle
 import org.springframework.hateoas.EntityModel
 import org.springframework.hateoas.Link
@@ -8,8 +7,9 @@ import org.springframework.hateoas.MediaTypes
 import org.springframework.hateoas.config.EnableHypermediaSupport
 import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder
 import org.springframework.hateoas.support.WebStack
-import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(produces = [MediaTypes.HAL_JSON_VALUE])
@@ -23,14 +23,5 @@ class IndexResource() {
     suspend fun index(): EntityModel<Unit> {
         return EntityModel.of(Unit, WebFluxLinkBuilder.linkTo(WebFluxLinkBuilder.methodOn(IndexResource::class.java).index()).withSelfRel().toMono().awaitSingle())
             .add(Link.of("http://start.spring.io").withRel(REL_SPRING_INITIALIZR))
-    }
-
-
-    @GetMapping(value = ["/person"])
-    suspend fun getPerson(@RequestParam @Validated name: String) = Person(name=name, email = "foo@bar.com")
-
-    @PostMapping(value = ["/person"])
-    suspend fun postPerson(@RequestBody @Validated person: Person): Person {
-        return person
     }
 }
